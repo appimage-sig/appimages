73b9f915b3e3c83cbc083aa26a29b16a21a079e0
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2026-05-21 14:18:01 +0300

    Update common.js

--- static/js/common.js
+++ static/js/common.js
@@ -85,33 +85,6 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 
 	/* ============================
-  // Testimonials Slider
-  ============================ */
-	if (document.querySelector(".my-slider")) {
-		var slider = tns({
-			container: ".my-slider",
-			items: 3,
-			slideBy: 1,
-			gutter: 20,
-			nav: false,
-			mouseDrag: true,
-			autoplay: false,
-			controlsContainer: "#customize-controls",
-			responsive: {
-				1024: {
-					items: 3,
-				},
-				768: {
-					items: 2,
-				},
-				0: {
-					items: 1,
-				},
-			},
-		});
-	}
-
-	/* ============================
   // Scroll to top
   ============================ */
 	const btnScrollToTop = document.querySelector(".top");
